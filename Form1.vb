0064f3e2ff1c3d2108e9bf721918bcc6008adb52
Aldrin Aragon <aldrin22aragon@gmail.com>
2023-05-05 01:37:51 +0800

    MsgBox show using button

--- Form1.vb
+++ Form1.vb
@@ -2,4 +2,8 @@
    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
       Me.Text = "Sample headeer"
    End Sub
+
+   Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+      MsgBox("Text show message box")
+   End Sub
 End Class
